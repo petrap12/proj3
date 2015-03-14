a19f1067a837299d90ee7336772a229355557582
Petra <petrap12@ru.is>
2015-03-14 13:50:37 +0000

    loginController added

---
+++ loginController.js
@@ -0,0 +1,46 @@
+"use strict";
+
+angular.module("MyApp", []);
+
+angular.module("MyApp").controller("CreateLoginController",
+function($scope, LoginResourse, toastr) {
+	$scope.thisUser = {
+		$scope.user = '';
+		$scope.pass = '';
+	};
+
+	$scope.onSave = function() {
+		LoginResource.createLogin($scope.thisUser);
+	};
+});
+
+angular.module("MyApp").factory("LoginResource",
+function($http) {
+	return {
+		createLogin: function(template){
+			//return $http.post()
+		}
+	};
+});
+
+/*angular.module('EvalClient').controller('LoginController', [
+'$scope', '$location', '$rootScope', '$routeParams', 'socket',    
+function ($scope, $location, $rootScope, $routeParams, socket) {
+	
+	$scope.errorMessage = '';
+	$scope.nickname = '';
+
+	$scope.login = [function() {			
+		if ($scope.nickname === '') {
+			$scope.errorMessage = 'Please choose a nick-name before continuing!';
+		} else {
+			socket.emit('adduser', $scope.nickname, ['available', function (available) {
+				if (available) {
+					$location.path('/rooms/' + $scope.nickname);
+				} else {
+					$scope.errorMessage = 'This nick-name is already taken!';
+				}
+			}]);			
+		}
+	}];
+}]);*/
