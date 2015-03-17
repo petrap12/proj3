560b2eb1704b3ab56d040128c97fe4adf8e6e9ed
Petra <petrap12@ru.is>
2015-03-17 11:18:01 +0000

    unit testing added

--- loginController.js
+++ loginController.js
@@ -18,7 +18,13 @@ angular.module("MyApp").factory("LoginResource",
 function($http) {
 	return {
 		createLogin: function(template){
-			//return $http.post()
+			$http.post("http://dispatch.hir.is/h14/api/v1/login", {user: $scope.username, pass: $scope.password})
+			.success (function(data) {
+			$scope.thisUser = data;
+			$scope.token = $scope.thisUser.Token;
+			console.log("here");
+			return true;
+		});
 
 		}
 	};
@@ -44,4 +50,22 @@ function ($scope, $location, $rootScope, $routeParams, socket) {
 			}]);			
 		}
 	}];
+}]);*/
+
+/*angular.module('MyApp', []);
+
+MyApp.$controller('loginController', ['$scope', '$http', function($scope, $http) {
+	$scope.username = '';
+	$scope.password = '';
+	$scope.token = '';
+
+	$scope.login = function() {
+		$http.post("http://dispatch.hir.is/h14/api/v1/login", {user: $scope.username, pass: $scope.password})
+		.success (function(data) {
+			$scope.thisUser = data;
+			$scope.token = $scope.thisUser.Token;
+			console.log("here");
+			return true;
+		});
+	};
 }]);*/
